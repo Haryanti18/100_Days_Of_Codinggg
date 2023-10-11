46e4a19debaff8a5e677cbdadf3336e6f06a9c5f
Haryanti <99654626+Haryanti18@users.noreply.github.com>
2023-10-11 19:26:29 +0800

    Add files via upload
    Day_2_Of_Coding

---
+++ Day_2.java
@@ -0,0 +1,17 @@
+package Coding;
+
+public class day_2 {
+
+    public static void main(String[] args) {
+        // Mendeklarasikan variabel
+        String nama = "Haryanti";
+        int umur = 21;
+        String tempat_kuliah = "Universitas Sulawesi Barat";
+
+        // Mencetak data
+        System.out.println("Nama saya adalah " + nama);
+        System.out.println("Umur saya adalah " + umur);
+        System.out.println("Saya kuliah di " + tempat_kuliah);
+    }
+}
+
